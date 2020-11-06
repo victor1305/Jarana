61d23e20f57d1dc672a89145581d62f4c94f6c5c
Victor Sanchez <victor1305@icloud.com>
2020-11-06 10:25:46 +0100

    [STABLE][v1.6]Change home photo and menu buttons

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import imagen from '../../images/jaranaback.png'
+import imagen from '../../images/fondo.jpg'
 
 const Home = () => {
     return (
         <div className = "image-container">
+            <h1 className = "home-title">Bar Cafetería Jarana</h1>
             <img className = "image" src = {imagen} alt = "imagen principal"/>
         </div>
     );
